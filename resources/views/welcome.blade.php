ae1decbe2d935c35e2d536437be0ca24b58a9922
Donkor T. Jonas <59501716+kwqme-Jonas@users.noreply.github.com>
2021-04-14 10:43:35 +0000

    edited welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,30 @@
-@extends('layouts.main')
+@extends('layouts.app')
 
 @section('content')
-<div class="container mx-auto px-4 py-16 flex flex-col md:flex-row">
-<div class="max-w-md py-4 px-8 bg-white bg-opacity-0 shadow-md rounded-lg my-20">
-  <div>
-    <h2 class="text-gray-800 text-3xl font-semibold">Design Tools</h2>
-    <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!</p>
-  </div>
-</div>
-<div class="md:ml-24 mt-64">
-<div class="flex  flex-col items-center justify-between">
-            @if (Route::has('login'))
-                <div class="">
-                    @auth
-                    <button >
-                         <a href="{{ url('/dashboard') }}">
-                         <button class="bg-white hover:bg-blue-700 text-gray-800 font-bold py-2 px-4 rounded-md">
-                           Dashboard
-                         </button>
-                         </a>
-                    </button>
-                        
-                    @else
+<div class="container mx-auto px-4 pt-16">
+  <img class="inset-0 absolute h-full w-full object-cover" src="img/home.png" alt="home">
+  <div class="absolute inset-0 bg-gray-900 bg-opacity-70"></div>
+    <div class="h-full flex-col text-center justify-center relative mt-64 sm:mt-45">
+    <h1 class="text-5xl font-semibold tracking-tight text-transparent bg-gradient-to-r bg-clip-text from-green-200 to-green-800 focus:outline-none focus:shadow-outline uppercase">Welcome to DefiatHMS</h1>
+    <div class="mt-6">
+    <p class="text-white font-semibold tracking-normal text-lg justify-center items-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, aspernatur. Id quam necessitatibus ullam dicta, perferendis earum asperiores voluptatem neque, officiis, maiores enim delectus. Sunt sint ducimus vitae debitis eum.</p>
+    </div>
 
-                        <a href="{{ route('login') }}">
-                            <button class="bg-white hover:bg-blue-700 text-gray-800 font-bold py-2 px-4 rounded-md">
-                            Login
-                            </button>
-                        </a>
+    <div class="mt-6">
+    <a href="{{ route('login') }}">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md">
+         Login
+      </button>
+     </a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">
-                            <button class="bg-white hover:bg-blue-700 text-gray-800 font-bold py-2 px-4 rounded-md">
-                                Register
-                            </button></a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-</div>
-</div>
+    <a href="{{ route('register') }}">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md">
+            Register
+        </button>
+    </a>
+    </div>
+    
+    
+    </div>
 </div>
 @endsection
-    
