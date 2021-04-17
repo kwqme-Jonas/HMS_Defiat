7ba66c33e8e786c663f11a3ebdce8b666296b754
Donkor T. Jonas <59501716+kwqme-Jonas@users.noreply.github.com>
2021-04-17 17:38:54 +0000

    updated student model

--- app/Models/Students.php
+++ app/Models/Students.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class Students extends Model
 {
     use HasFactory;
+
+
+
+    protected $fillable = [
+        'surname',
+        'othername',
+        'appNumber',
+        'indexnum',
+        'dob',
+        'contact',
+        'region',
+        'year',
+        'level',
+        'programme',
+        'department',
+        'hall',
+        'fName',
+        'mName',
+        'fNumber',
+        'mNumber',
+        'pic',
+        'sponsorname',
+        'sponsor_add',
+        'sponsor_contact',
+        'sponsor_email',
+    ];
+
 }
