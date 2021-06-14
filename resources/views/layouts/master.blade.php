36f7ef32f91d43b920d03e13d2a69c85ddb93c82
Donkor T. Jonas <59501716+kwqme-Jonas@users.noreply.github.com>
2021-06-14 09:25:43 +0000

    added admin dashboard

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ $page->language ?? 'en' }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="referrer" content="always">
+
+          <!-- Styles -->
+          <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        <!-- Scripts -->
+            <script src="{{ asset('js/app.js') }}" defer></script>
+      
+    </head>
+    <body>
+        <div x-data="{ sidebarOpen: false }" class="flex h-screen bg-gray-200 font-roboto">
+            @include('layouts.sidebar')
+            
+            <div class="flex-1 flex flex-col overflow-hidden">
+                @include('layouts.header')
+
+                <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+                    <div class="container mx-auto px-6 py-8">
+                        @yield('body')
+                    </div>
+                </main>
+            </div>
+        </div>
+    </body>
+</html>
